bc7a40ff05e8b6aeeabfb1eb9d7894e761cb9015
mostafaosmanNodejs <85788565+mostafaosmanNodejs@users.noreply.github.com>
2023-12-31 18:14:31 +0300

    Message Broker Settings Configuration

---
+++ API_Publisher/MessageBrokerSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Publisher;
+
+public class MessageBrokerSettings
+{
+    public string Host { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
--- API_Publisher/Program.cs
+++ API_Publisher/Program.cs
@@ -1,9 +1,17 @@
+using API_Publisher;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Message Broker Settings Configuration 
+builder.Services.Configure<MessageBrokerSettings>(
+    builder.Configuration.GetSection("MessageBroker"));
+builder.Services.AddSingleton(sp=>
+sp.GetRequiredService<MessageBrokerSettings>());
+///
 
 var app = builder.Build();
 
